facf1e8dc6b3ccf6ad45d1e6a27ab3c4404893cc
idorenyinudoh <idorenyinudoh10@outlook.com>
2021-08-25 11:18:59 +0100

    add heal player functionality

--- app.js
+++ app.js
@@ -13,18 +13,28 @@ const app = Vue.createApp({
     methods: {
         attackMonster() {
             this.currentRound++;
-            const attackValue = getRandomValue(5, 12)
+            const attackValue = getRandomValue(5, 12);
             this.monsterHealth -= attackValue;
             this.attackPlayer();
         },
         attackPlayer() {
-            const attackValue = getRandomValue(8, 15)
+            const attackValue = getRandomValue(8, 15);
             this.playerHealth -= attackValue;
         },
         specialAttackMonster() {
             this.currentRound++;
-            const attackValue = getRandomValue(10, 25)
+            const attackValue = getRandomValue(10, 25);
             this.monsterHealth -= attackValue;
+            this.attackPlayer();
+        },
+        healPlayer() {
+            this.currentRound++;
+            const healValue = getRandomValue(8, 20);
+            if(this.playerHealth + healValue > 100) {
+                this.playerHealth = 100;
+            } else {
+                this.playerHealth += healValue;
+            }
             this.attackPlayer();
         }
     },

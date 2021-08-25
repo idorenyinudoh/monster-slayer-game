3db25881b588a4f187001c77adc51087522bf965
idorenyinudoh <idorenyinudoh10@outlook.com>
2021-08-25 11:54:30 +0100

    add battle log functionality

--- app.js
+++ app.js
@@ -8,7 +8,8 @@ const app = Vue.createApp({
             playerHealth: 100,
             monsterHealth: 100,
             currentRound: 0,
-            winner: null
+            winner: null,
+            logMessages: []
         }
     },
     methods: {
@@ -16,16 +17,19 @@ const app = Vue.createApp({
             this.currentRound++;
             const attackValue = getRandomValue(5, 12);
             this.monsterHealth -= attackValue;
+            this.addLogMessages('player', 'attack', attackValue);
             this.attackPlayer();
         },
         attackPlayer() {
             const attackValue = getRandomValue(8, 15);
             this.playerHealth -= attackValue;
+            this.addLogMessages('monster', 'attack', attackValue);
         },
         specialAttackMonster() {
             this.currentRound++;
             const attackValue = getRandomValue(10, 25);
             this.monsterHealth -= attackValue;
+            this.addLogMessages('player', 'attack', attackValue);
             this.attackPlayer();
         },
         healPlayer() {
@@ -36,16 +40,25 @@ const app = Vue.createApp({
             } else {
                 this.playerHealth += healValue;
             }
+            this.addLogMessages('player', 'heal', healValue);
             this.attackPlayer();
         },
         startGame() {
             this.playerHealth = 100;
             this.monsterHealth = 100;
             this.winner = null;
-            this.currentRound = 0
+            this.currentRound = 0;
+            this.logMessages = [];
         },
         surrender() {
             this.winner = 'monster';
+        },
+        addLogMessages(who, what, value) {
+            this.logMessages.unshift({
+                actionBy: who,
+                actionType: what,
+                actionValue: value
+            });
         }
     },
     computed: {

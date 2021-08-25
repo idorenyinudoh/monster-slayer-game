1e8563b12363855c535e2014c44197dbcbc1cc72
idorenyinudoh <idorenyinudoh10@outlook.com>
2021-08-25 11:13:37 +0100

    add special attack functionality

--- app.js
+++ app.js
@@ -6,11 +6,13 @@ const app = Vue.createApp({
     data() {
         return {
             playerHealth: 100,
-            monsterHealth: 100
+            monsterHealth: 100,
+            currentRound: 0
         }
     },
     methods: {
         attackMonster() {
+            this.currentRound++;
             const attackValue = getRandomValue(5, 12)
             this.monsterHealth -= attackValue;
             this.attackPlayer();
@@ -18,6 +20,12 @@ const app = Vue.createApp({
         attackPlayer() {
             const attackValue = getRandomValue(8, 15)
             this.playerHealth -= attackValue;
+        },
+        specialAttackMonster() {
+            this.currentRound++;
+            const attackValue = getRandomValue(10, 25)
+            this.monsterHealth -= attackValue;
+            this.attackPlayer();
         }
     },
     computed: {
@@ -26,6 +34,9 @@ const app = Vue.createApp({
         },
         playerBarStyles() {
             return {width: this.playerHealth + '%'}
+        },
+        mayUseSpecialAttack() {
+            return this.currentRound % 3 !== 0;
         }
     }
 });
